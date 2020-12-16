ba92955d34970c0d9f9aee886e571ae13123a5f3
Saikat Adhikari <saikata95@gmail.com>
2020-12-17 02:05:19 +0530

    Added cors middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 require('./db/mongoose');
 const battleRouter = require('./router/battle');
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(battleRouter);
 
